4b2bcf4e246d87c172d073055b42862d7b5c5194
Strice91 <r.strice@gmail.com>
2014-10-19 12:46:52 +0200

    cilent

--- WebServer/client.py
+++ WebServer/client.py
@@ -3,9 +3,12 @@ import sys
 
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-host="127.0.0.1"
-port=8080
+host="192.168.178.23"
+port=8075
 s.connect((host,port))
-s.send("test".encode("UTF-8"))
-
+s.send("USER meinName\r\nPASS meinPass\r\n".encode("UTF-8"))
+while 1:
+	msg = s.recv(1024).decode()
+	if msg:
+		print(msg)
 s.close()
